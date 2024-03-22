4b9f5c3be3f54a4639da95d694264b4c0f198409
JyukyoMaeda <maeda-r@jyukyo.com>
2024-03-22 20:18:22 +0900

    完成

--- js/video.js
+++
@@ -1,8 +0,0 @@
-
-    // 動画アイテム(.hv-item)
-    const hoverVideos = document.querySelectorAll(".hv-item");
-    hoverVideos.forEach((hoverVideo) => {
-        hoverVideo.addEventListener("mouseover", () => hoverVideo.play());
-        hoverVideo.addEventListener("mouseout", () => hoverVideo.pause());
-    });
-
